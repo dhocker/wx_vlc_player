22b9cca9719ec21eb20c7b6c2d3fd0b38e982e40
Dave Hocker <AtHomeX10@gmail.com>
2023-04-12 15:21:17 -0500

    Add command argument to control verison updating

--- build_app.sh
+++ build_app.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-# Build macOS X version of pyid3tag app
+# Build macOS X version of wxVLCPlayer app
 
 source `which virtualenvwrapper.sh`
 workon vlc3
-python increment_version.py
+
+if [[ $# == 1 ]] && [[ $1 == "-incr" ]]; then
+  echo "Increment build number"
+  python increment_version.py
+fi
+
 python setup.py py2app
 deactivate
